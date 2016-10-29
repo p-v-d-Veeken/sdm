462b34113de7788a20a5b84ea59e886473325dfb
Pieter <pietervanderveeken@gmail.com>
2016-10-29 13:11:06 +0200

    Started on encrypted search

--- main/src/main/java/com/tudelft/sdm/service/CompareService.java
+++ main/src/main/java/com/tudelft/sdm/service/CompareService.java
@@ -1,11 +1,25 @@
 package com.tudelft.sdm.service;
 
+import com.tudelft.comparison.Comparator;
+import com.tudelft.paillier.EncryptedNumber;
+import com.tudelft.paillier.PaillierContext;
 import com.tudelft.paillier.PaillierPrivateKey;
 import com.tudelft.sdm.service.enumerations.ComparisonResultEnumeration;
+import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
 
-public interface CompareService
+@Service(value = "compareService")
+class CompareService
 {
-	ComparisonResultEnumeration compare(BigInteger a, BigInteger b, PaillierPrivateKey sk);
+	static ComparisonResultEnumeration compare(BigInteger a, BigInteger b, PaillierPrivateKey sk)
+	{
+		Comparator      comp = new Comparator(sk);
+		PaillierContext cxt  = sk.getPublicKey().createSignedContext();
+		EncryptedNumber aEnc = new EncryptedNumber(cxt, a, 0);
+		EncryptedNumber bEnc = new EncryptedNumber(cxt, b, 0);
+		BigInteger      res  = comp.compare(aEnc, bEnc).decrypt(sk).decodeBigInteger();
+		
+		return ComparisonResultEnumeration.fromResult(res);
+	}
 }
--- main/src/main/java/com/tudelft/sdm/service/RecordServiceImpl.java
+++ main/src/main/java/com/tudelft/sdm/service/RecordServiceImpl.java
@@ -1,68 +1,156 @@
 package com.tudelft.sdm.service;
 
+import com.tudelft.paillier.PaillierPrivateKey;
 import com.tudelft.sdm.persistence.Client;
 import com.tudelft.sdm.persistence.Record;
 import com.tudelft.sdm.persistence.dao.RecordRepository;
+import com.tudelft.sdm.service.enumerations.ComparisonResultEnumeration;
 import io.swagger.model.ApiRecord;
 import io.swagger.model.Keyring;
 import io.swagger.model.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.apache.commons.codec.binary.Base64;
 
+import java.math.BigInteger;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service(value = "recordService")
-public class RecordServiceImpl implements RecordService {
-
-    private RecordRepository recordDao;
-    private ClientService clientService;
-
-    @Autowired
-    public void setRecordDao(RecordRepository recordDao, ClientService clientService) {
-        this.recordDao = recordDao;
-        this.clientService = clientService;
-    }
-
-    public Record find(int recordId, int clientId, Keyring keyring) {
-        Record record = recordDao.findByIdAndClientId((long) recordId, (long) clientId);
-        if (record == null) {
-            throw new NullPointerException();
-        }
-        return record;
-    }
-
-    @Override
-    public Void create(int clientId, ApiRecord apiRecord, Keyring keyring) {
-        Client client = clientService.find(clientId, keyring);
-        Record record = new Record(apiRecord);
-        record.setCreatedAt(new Date());
-        record.setUpdatedAt(new Date());
-        record.setClient(client);
-        recordDao.save(record);
-        return null;
-    }
-
-    @Override
-    public Void update(int recordId, int clientId, ApiRecord apiRecord, Keyring keyring) {
-        Record record = this.find(recordId, clientId, keyring);
-        record.merge(apiRecord);
-        record.setUpdatedAt(new Date());
-        recordDao.save(record);
-        return null;
-    }
-
-    @Override
-    public Void delete(int recordId, int clientId, Keyring keyring) {
-        Record record = this.find(recordId, clientId, keyring);
-        recordDao.delete(record);
-        return null;
-    }
-
-    @Override
-    public List<Record> find(int clientId, List<Query> query, Keyring keyring) {
-        Client client = clientService.find(clientId, keyring);
-        // TODO filter records
-        return client.getRecords();
-    }
+public class RecordServiceImpl implements RecordService
+{
+	
+	private RecordRepository recordDao;
+	private ClientService    clientService;
+	
+	@Autowired
+	public void setRecordDao(RecordRepository recordDao, ClientService clientService)
+	{
+		this.recordDao = recordDao;
+		this.clientService = clientService;
+	}
+	
+	public Record find(int recordId, int clientId, Keyring keyring)
+	{
+		Record record = recordDao.findByIdAndClientId((long) recordId, (long) clientId);
+		if (record == null)
+		{
+			throw new NullPointerException();
+		}
+		return record;
+	}
+	
+	@Override
+	public Void create(int clientId, ApiRecord apiRecord, Keyring keyring)
+	{
+		Client client = clientService.find(clientId, keyring);
+		Record record = new Record(apiRecord);
+		record.setCreatedAt(new Date());
+		record.setUpdatedAt(new Date());
+		record.setClient(client);
+		recordDao.save(record);
+		return null;
+	}
+	
+	@Override
+	public Void update(int recordId, int clientId, ApiRecord apiRecord, Keyring keyring)
+	{
+		Record record = this.find(recordId, clientId, keyring);
+		record.merge(apiRecord);
+		record.setUpdatedAt(new Date());
+		recordDao.save(record);
+		return null;
+	}
+	
+	@Override
+	public Void delete(int recordId, int clientId, Keyring keyring)
+	{
+		Record record = this.find(recordId, clientId, keyring);
+		recordDao.delete(record);
+		return null;
+	}
+	
+	@Override
+	public List<Record> find(int clientId, List<Query> queries, Keyring keyring)
+	{
+		Client             client = clientService.find(clientId, keyring);
+		PaillierPrivateKey sk     = PaillierPrivateKey.create(2048); //TODO: daadwerkelijke sk getten
+		
+		return client.getRecords()
+				.stream()
+				.parallel()
+				.filter(record -> queries.stream()
+						.parallel()
+						.allMatch(query -> recordMatchesQuery(record, query, sk))
+				).collect(Collectors.toList());
+	}
+	
+	private boolean recordMatchesQuery(Record record, Query query, PaillierPrivateKey sk)
+	{
+		return query.getColumn().equals(Query.ColumnEnum.KEY)
+		       ? evaluateKeyQuery(record, query, sk)
+		       : evaluateValueQuery(record, query, sk);
+	}
+	
+	private boolean evaluateKeyQuery(Record record, Query query, PaillierPrivateKey sk)
+	{
+		List<BigInteger> recordKeys = Pattern.compile(" ").splitAsStream(record.getName())
+				.map(str -> new BigInteger(Base64.decodeBase64(str)))
+				.collect(Collectors.toList());
+		List<BigInteger> queryValues = Pattern.compile(" ").splitAsStream(query.getValue())
+				.map(str -> new BigInteger(Base64.decodeBase64(str)))
+				.collect(Collectors.toList());
+		
+		if (query.getOperator().equals(Query.OperatorEnum.EQUAL))
+		{
+			if (recordKeys.size() != queryValues.size()) { return false; }
+			
+			return IntStream.range(0, recordKeys.size())
+					.parallel()
+					.allMatch(i -> CompareService
+							.compare(recordKeys.get(i), queryValues.get(i), sk)
+							.equals(ComparisonResultEnumeration.EQUAL));
+		}
+		int windowSize = queryValues.size();
+		
+		if (windowSize > recordKeys.size()) { return false; }
+		
+		return IntStream.range(0, recordKeys.size())
+				.parallel()
+				.anyMatch(i ->
+				{
+					List<BigInteger> window = recordKeys.subList(i, windowSize);
+					return IntStream.range(0, queryValues.size())
+							.parallel()
+							.allMatch(j -> CompareService
+									.compare(queryValues.get(j), window.get(j), sk)
+									.equals(ComparisonResultEnumeration.EQUAL));
+				});
+	}
+	
+	private boolean evaluateValueQuery(Record record, Query query, PaillierPrivateKey sk)
+	{
+		BigInteger                  recordValue = new BigInteger(Base64.decodeBase64(record.getValue()));
+		BigInteger                  queryValue  = new BigInteger(Base64.decodeBase64(query.getValue()));
+		ComparisonResultEnumeration res         = CompareService.compare(recordValue, queryValue, sk);
+		
+		switch (query.getOperator())
+		{
+			case LESS_THAN:
+				return res.equals(ComparisonResultEnumeration.SMALLER);
+			case LESS_THAN_OR_EQUAL_TO:
+				return res.equals(ComparisonResultEnumeration.SMALLER) || res.equals(ComparisonResultEnumeration.EQUAL);
+			case EQUAL:
+				return res.equals(ComparisonResultEnumeration.EQUAL);
+			case GREATER_THAN:
+				return res.equals(ComparisonResultEnumeration.GREATER);
+			case GREATER_THAN_OR_EQUAL_TO:
+				return res.equals(ComparisonResultEnumeration.GREATER) || res.equals(ComparisonResultEnumeration.EQUAL);
+			default:
+				throw new IllegalArgumentException();
+		}
+	}
 }
